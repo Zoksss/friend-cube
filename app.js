3aa36510fc435691723d05b08de19abf6434c983
Zoksss <zorantodorovic80@gmail.com>
2021-12-04 22:36:33 +0100

    /

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 // App setup
 const PORT = process.env.PORT || 3000;
 const app = express();
-const server = app.listen(PORT , "192.168.1.11", function () {
+const server = app.listen(PORT, function () {
     console.log(`Listening on port ${PORT}`);
     console.log(`http://localhost:${PORT}`);
 });
