9592a6c05941eec63f54b34f36c994576c8eedc2
Zoran Todorovic <49413461+Zoksss@users.noreply.github.com>
2022-03-26 01:17:49 +0100

    pyra scramble fix

--- app.js
+++ app.js
@@ -180,7 +180,7 @@ const generateScramble = (puzzle) => {
     }
     else if(puzzle === "pyra"){
         let scrambele = "";
-        for (a = y = r = '', x = Math.random; a++ < 9; scrambele += (r + " '2"[0 | x(y = r) * 3] + ' '))
+        for (a = y = r = '', x = Math.random; a++ < 9; scrambele += (r + " '"[0 | x(y = r) * 2] + ' '))
             for (; r == y; r = 'BRUL'[0 | x() * 4]);
         for(let i = 0; i < 4; i++){
             let l = Math.floor(Math.random() * 2);
